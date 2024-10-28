e7d6c4861f13f19aac6b39e53f5e029081cbd6ee
EresDev <eresdev@hotmail.com>
2024-10-28 14:19:00 +0500

    fix: make tests work with mocked reloadly

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 
 export default defineWorkersConfig({
+  resolve: {
+    alias: [{ find: "msw/node", replacement: "/node_modules/msw/lib/native/index.mjs" }],
+  },
   test: {
-    dir: "tests",
+    //setupFiles: ["/tests/setup.ts"],
+    dir: "tests/unit",
     poolOptions: {
       workers: {
         wrangler: { configPath: "./wrangler.toml" },
