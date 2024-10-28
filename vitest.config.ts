efcc0a23130ff327713f3281c661a075c727819c
EresDev <eresdev@hotmail.com>
2024-10-28 14:44:37 +0500

    docs: add reason for alias in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 
 export default defineWorkersConfig({
   resolve: {
+    // To deal with "Error: No known conditions for "./node" specifier in "msw" package"
+    // From https://github.com/solidjs/vite-plugin-solid/issues/125
     alias: [{ find: "msw/node", replacement: "/node_modules/msw/lib/native/index.mjs" }],
   },
   test: {
