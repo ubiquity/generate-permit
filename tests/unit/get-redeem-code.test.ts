6932b77bd44314c93318dfac3b252a9b393e2f1e
EresDev <eresdev@hotmail.com>
2024-11-01 14:01:37 -0400

    test: give redeem code err on empty tx id

--- tests/unit/get-redeem-code.test.ts
+++ tests/unit/get-redeem-code.test.ts
@@ -57,4 +57,19 @@ describe("Get redeem code", () => {
     expect(response.status).toBe(403);
     expect(await response.json()).toEqual({ message: "Given details are not valid to redeem code." });
   });
+
+  it("should return err when transaction id is empty", async () => {
+    const transactionId = "";
+    const signedMessage =
+      "0x78870c5b97821f4d828d5fea945e331edb0c5938e0820910e7fccaead9179e3030a5d80e123c1e335005b4cc81dbd6509f1a31b71ee18074b8c70aeacad666351b";
+    const wallet = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    const permitSig = "0x4599c64a7e7556976972d50d961058280e752e1d824db0201305852e80601ed51e6a8bbb71047d09e8d4c75d450df1fc073c775426ee13f2006b8ad55ca2e49d1c";
+
+    const path = `/get-redeem-code?transactionId=${transactionId}&signedMessage=${signedMessage}&wallet=${wallet}&permitSig=${permitSig}`;
+    const eventCtx = createEventContext(path, execContext);
+    const response = await pagesFunction(eventCtx);
+    await waitOnExecutionContext(execContext);
+    expect(response.status).toBe(403);
+    expect(await response.json()).toEqual({ message: "Given details are not valid to redeem code." });
+  });
 });
